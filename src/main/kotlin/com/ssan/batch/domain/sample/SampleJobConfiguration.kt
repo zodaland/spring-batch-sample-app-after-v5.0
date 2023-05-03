8f2e82dc5784e0266eea0cccd23ca03199ca07f3
zodaland <admin@zodaland.com>
2023-05-03 14:43:05 +0900

    테스트 최적화를 위한 코드 리팩토링

--- src/main/kotlin/com/ssan/batch/domain/sample/SampleJobConfiguration.kt
+++ src/main/kotlin/com/ssan/batch/domain/sample/SampleJobConfiguration.kt
@@ -2,41 +2,48 @@ package com.ssan.batch.domain.sample
 
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
-class SampleJobConfiguration (
-    private val jobRepository: JobRepository,
-    private val transactionManager: PlatformTransactionManager,
-    private val tasklet: SampleTasklet
-) {
+class SampleJobConfiguration {
+    // Configuration 클래스 내 Job이 두개 이상 일 경우 test시 JobLauncherTestUtils에서 setJob을 통해 자동으로 Job을 등록 하지 못한다.
+    /*
     @Bean
-    fun job(): Job {
+    fun job(jobRepository: JobRepository, step: Step): Job {
         return JobBuilder("simpleJob1", jobRepository)
-            .start(step())
+            .start(step)
             .build()
     }
     @Bean
-    fun step(): Step {
+    fun step(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
         return StepBuilder("simpleStep1", jobRepository)
-            .tasklet(tasklet, transactionManager)
+            .tasklet(sampleTasklet(), transactionManager)
             .build()
     }
 
     @Bean
-    fun job2(): Job {
+    @StepScope
+    fun sampleTasklet(): Tasklet {
+        return SampleTasklet()
+    }
+    */
+
+    @Bean
+    fun job2(jobRepository: JobRepository, step2: Step): Job {
         return JobBuilder("simpleJob2", jobRepository)
-            .start(step2())
+            .start(step2)
             .build()
     }
     var count: Int = 0
     @Bean
-    fun step2(): Step {
+    fun step2(jobRepository: JobRepository, transactionManager: PlatformTransactionManager): Step {
         return StepBuilder("simpleStep2", jobRepository)
             .chunk<String, String>(1, transactionManager)
             .reader {  ->
